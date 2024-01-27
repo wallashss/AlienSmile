32f0396196cbe10ece0468483e164ed44d96d2ac
Wallas Henrique <wallashss@tecgraf.puc-rio.br>
2024-01-27 08:58:27 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AlienSmile
+
+Developed with Unreal Engine 5
---
+++ Source/AlienSmile/AlienSmileGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AlienSmileGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALIENSMILE_API AAlienSmileGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
